410f97b5ae30c766e8ce51408afcb6b1fc11626e
mli <muli@cs.cmu.edu>
2018-11-30 23:25:12 +0000

    mv card.js card.css into src/

--- src/js/sphinx_materialdesign_theme.js
+++ src/js/sphinx_materialdesign_theme.js
@@ -115,4 +115,26 @@ $(function() {
     const adjust = new AdjustHeight();
 
     $('.mdl-layout__content').focus();
+
+    $('.mx-card').each(function(){
+        $(this).addClass('mdl-card mdl-shadow--2dp');
+    });
+    $('.mx-card .mx-card-title').each(function(){
+        $(this).addClass('mdl-card__title');
+    });
+    $('.mx-card .mx-card-text').each(function(){
+        $(this).addClass('mdl-card__supporting-text');
+    });
+    $('.mx-card-link').each(function(){
+        $(this).hide();
+    });
+    $('.mdl-card').each(function(){
+        $(this).click(function() {
+            var url = $(this).find('.mx-card-link').text();
+            if (url) {
+                window.location = url;
+            }
+            return true;
+        });
+    });
 });
