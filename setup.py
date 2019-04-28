b95556477d649ae5ff787028b92a6ba0cf7b7529
muli <muli@cs.cmu.edu>
2019-04-27 22:56:43 -0700

    slightly update pkg info

--- setup.py
+++ setup.py
@@ -5,27 +5,15 @@ setup(
     name = 'mxtheme',
     version = __version__,
     author = 'Mu Li',
-    author_email= 'mli@amazon.com',
+    author_email= '',
     url="https://github.com/mli/mx-theme",
-    description='MXNet Website Theme, adapted from Sphinx Material Design Theme',
+    description='A Sphinx theme based on Material Design, adapted from sphinx_materialdesign_theme',
     packages = ['mxtheme'],
     include_package_data=True,
     license= 'MIT License',
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "Intended Audience :: System Administrators",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Topic :: Internet",
-        "Topic :: Software Development :: Documentation"
-    ],
     entry_points = {
         'sphinx.html_themes': [
             'mxtheme = mxtheme',
         ]
     },
 )
-
