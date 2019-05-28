e4b490d2acf1d60f36d5a96303f4c8988fa73428
muli <muli@cs.cmu.edu>
2019-05-28 16:10:04 -0700

    bump to 0.3.7

--- mxtheme/__init__.py
+++ mxtheme/__init__.py
@@ -1,7 +1,7 @@
 from os import path
 from .card import CardDirective
 
-__version__ = '0.3.6'
+__version__ = '0.3.7'
 __version_full__ = __version__
 
 package_dir = path.dirname(path.abspath(__file__))
