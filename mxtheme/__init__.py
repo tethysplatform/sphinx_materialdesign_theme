5457b9f3f88140ccfca653eaa7174901b8fe721a
muli <muli@cs.cmu.edu>
2019-11-05 10:23:02 -0800

    add role=main to fix search bug in sphinx 2.0+

--- mxtheme/__init__.py
+++ mxtheme/__init__.py
@@ -1,7 +1,7 @@
 from os import path
 from .card import CardDirective
 
-__version__ = '0.3.9'
+__version__ = '0.3.10'
 __version_full__ = __version__
 
 package_dir = path.dirname(path.abspath(__file__))
